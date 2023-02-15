ca4a123b117e542c6bb57e1a97611f789da0da31
mochan42 <mochan@students.42wolfsburg.de>
2023-02-15 18:00:39 +0100

    test

---
+++ CPP0/ex02/Account.cpp
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moninechan <moninechan@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/05 12:18:08 by moninechan        #+#    #+#             */
+/*   Updated: 2023/02/05 13:08:16 by moninechan       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include <iostream>
+#include <string>
+
+Account::Account(int initial_deposit)
+{
+
+}
+
+Account::~Account()
+{
+    
+}
+
+int	Account::getNbAccounts(void)
+{
+    return _nbAccounts;
+}
+
+int	Account::getTotalAmount(void)
+{
+    return _totalAmount;
+}
+
+int	Account::getNbDeposits(void)
+{
+    return _nbDeposits;
+}
+
+int	Account::getNbWithdrawals(void)
+{
+    return _nbWithdrawals;
+}
+
+void	Account::displayAccountsInfos(void)
+{
+    
+}
+
+void	makeDeposit(int deposit)
+{
+
+}
+
+bool	makeWithdrawal(int withdrawal)
+{
+    
+}
+
+int		checkAmount(void) const
+{
+
+}
+
+void	displayStatus(void) const
+{
+    
+}
+
+static void	_displayTimestamp( void )
+{
+
+}
